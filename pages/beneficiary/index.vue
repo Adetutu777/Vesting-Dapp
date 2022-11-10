fe7952e27a3acf380e86b2180cb6654aad4a4f57
Adetutu <adetutugbangbola@gmail.com>
2022-11-10 21:59:32 +0100

    update

---
+++ pages/beneficiary/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+       <p>Beneficiary address</p>
+       <p>Amount</p>
+
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        setup(){
+
+                
+
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,46 @@
 <template>
 <div class="">
-    <div class="container mt-5">
-        <!-- <ValidationObserver> -->
+   
 
+    <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item  to="../beneficiary">Beneficiary</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+        <!-- <ValidationObserver> -->
+ <div class="container mt-5">
         <form class="mb-5 mt-0" @submit.prevent="createScheduler">
 
             <label class="mt-3">Wallet Address:</label>
@@ -51,15 +89,15 @@
         </form>
         <!-- </ValidationObserver> -->
     </div>
+
+
+
 </div>
 </template>
 
 <script>
 import {
-    ref,
-    onMounted,
-    reactive
-} from '@nuxtjs/composition-api';
+    ref,onMounted,reactive,} from '@nuxtjs/composition-api';
 import bunzz from 'bunzz-sdk';
 import {
     DAPP_ID,
